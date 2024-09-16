9826483282ccb7727bba039e8e3b2bd8c2514d50
Samuel Tetteh <samuelamoahtetteh@gmail.com>
2024-09-16 16:15:53 +0000

    hide read more link and action card css

--- src/components/home/ActionCard.tsx
+++ src/components/home/ActionCard.tsx
@@ -1,4 +1,3 @@
-import { ArrowRight24Filled } from "@fluentui/react-icons";
 import React from "react";
 
 interface ActionCardProps {}
@@ -8,17 +7,16 @@ const ActionCard: React.FC<ActionCardProps> = ({}) => {
     <div className="action-stories">
       <div className="action-top">STORIES</div>
       <div className="action-card-title">NUTS FOR GROWTH</div>
-      <div className="action-card-body">
+      <div className="action-card-body" style={{ marginBottom: 71 }}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat.
       </div>
 
-      <button className="flex flex-row items-center justify-center action-link">
+      {/* <button className="flex flex-row items-center justify-center action-link">
         <p className="action-card-link">Read more</p>{" "}
-        <ArrowRight24Filled color="#62b46a" />
-      </button>
+      </button> */}
     </div>
   );
 };
