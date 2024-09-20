7c1011a42ab60359d7a23cbbf3f7a6676fbd5dca
Samuel Tetteh <samuelamoahtetteh@gmail.com>
2024-09-20 10:49:06 +0000

    message fix

--- src/data/about.tsx
+++ src/data/about.tsx
@@ -13,7 +13,7 @@ export const rings = [
   },
   {
     title: "Managing investments with unique principles",
-    body: "we are extremely long term oriented because we believe that patience and a long-term horizon are key ingredients for building enduring agribusiness at scale in west Africa. We are rigid on vision but flexible on approach, reflected by our flexible capital allocation which we tailor to the specific needs of a portfolio company. We in the growth of our portfolio companies, providing key strategic support and resources. We are community centric, intentionally engage with, and support stakeholders across the value chain, to strengthen business relationships and create a strong ecosystem for our portfolio companies to thrive. And we charge no annual management fees.",
+    body: "We are extremely long term oriented because we believe that patience and a long-term horizon are key ingredients for building enduring agribusiness at scale in west Africa. We are rigid on vision but flexible on approach, reflected by our flexible capital allocation which we tailor to the specific needs of a portfolio company. We in the growth of our portfolio companies, providing key strategic support and resources. We are community centric, intentionally engage with, and support stakeholders across the value chain, to strengthen business relationships and create a strong ecosystem for our portfolio companies to thrive. And we charge no annual management fees.",
   },
   {
     title: "Achieving impact at all levels",
